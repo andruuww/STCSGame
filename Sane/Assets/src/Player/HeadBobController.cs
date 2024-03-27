ca17a6f3cd778fd51d48f72c5d97dfef15e88cc7
nguyen andrew <0427andrewnguyen@gmail.com>
2024-03-27 12:33:27 -0700

    improved a little

--- Sane/Assets/src/Player/HeadBobController.cs
+++ Sane/Assets/src/Player/HeadBobController.cs
@@ -3,18 +3,25 @@ using UnityEngine;
 public class HeadBobController : MonoBehaviour {
     [SerializeField] private bool enable = true;
 
-    [SerializeField] [Range(0, 0.1f)] private float amplitude = 0.015f;
-    [SerializeField] [Range(0, 30)] private float frequency = 10.0f;
+    [SerializeField] [Range(0, 50f)] private float lerpSpeed = 10f;
+    [SerializeField] [Range(0, 100f)] private float decaySpeed = 10f;
+
+    [SerializeField] [Range(0, 0.1f)] private float walkingAmp = 0.0003f;
+    [SerializeField] [Range(0, 30)] private float walkingFreq = 10.0f;
+
+    [SerializeField] [Range(0, 0.1f)] private float runningAmp = 0.03f;
+    [SerializeField] [Range(0, 30)] private float runningFreq = 20.0f;
 
     [SerializeField] private new Transform camera;
-    [SerializeField] private Transform cameraHolder;
-    [SerializeField] private Rigidbody rb;
+    public InputManager inputManager;
 
-    private readonly float toggleSpeed = 1.0f;
-    private Vector3 startPos;
+    private float _currentAmp;
+    private float _currentFreq;
+
+    private Vector3 _startPos;
 
     private void Awake() {
-        startPos = camera.localPosition;
+        _startPos = camera.localPosition;
     }
 
     private void Update() {
@@ -23,36 +30,54 @@ public class HeadBobController : MonoBehaviour {
         CheckMotion();
         ResetPosition();
         // camera.LookAt(FocusTarget());
+
+        print(_currentAmp);
     }
 
 
-    private Vector3 FootStepMotion() {
+    private Vector3 FootStepMotion(float amp, float freq) {
         Vector3 pos = Vector3.zero;
-        pos.y += Mathf.Sin(Time.time * frequency) * amplitude;
-        pos.x += Mathf.Cos(Time.time * frequency / 2) * amplitude * 1;
+        pos.y += Mathf.Sin(Time.time * freq) * amp;
+        pos.x += Mathf.Cos(Time.time * freq / 2) * amp * 1;
         return pos;
     }
 
     private void CheckMotion() {
-        float speed = new Vector3(rb.velocity.x, 0, rb.velocity.z).magnitude;
+        switch (inputManager.GetPlayerState()) {
+            case PlayerState.Walking:
+                _currentFreq = walkingFreq;
+                LerpAmp(walkingAmp);
+                break;
+            case PlayerState.Running:
+                _currentFreq = runningFreq;
+                LerpAmp(runningAmp);
+                break;
+            default:
+                _currentFreq = 0;
+                LerpAmp(0);
+                break;
+        }
 
-        if (speed < toggleSpeed) return;
-
-        PlayMotion(FootStepMotion());
+        PlayMotion(FootStepMotion(_currentAmp, _currentFreq));
     }
 
     private void ResetPosition() {
-        if (camera.localPosition == startPos) return;
-        camera.localPosition = Vector3.Lerp(camera.localPosition, startPos, 1 * Time.deltaTime);
+        if (camera.localPosition == _startPos) return;
+        camera.localPosition = Vector3.Lerp(camera.localPosition, _startPos, 1 * Time.deltaTime);
     }
 
-    private Vector3 FocusTarget() {
-        Vector3 pos = new(transform.position.x, transform.position.y + cameraHolder.localPosition.y,
-            transform.position.z);
-
-        pos += cameraHolder.forward * 15.0f;
-        return pos;
+    private void LerpAmp(float targetAmp) {
+        float speed = _currentAmp > targetAmp ? decaySpeed : lerpSpeed;
+        _currentAmp = Mathf.Lerp(_currentAmp, targetAmp, Time.deltaTime * speed);
     }
+
+    // private Vector3 FocusTarget() {
+    //     Vector3 pos = new(transform.position.x, transform.position.y + cameraHolder.localPosition.y,
+    //         transform.position.z);
+    //
+    //     pos += cameraHolder.forward * 15.0f;
+    //     return pos;
+    // }
 
     private void PlayMotion(Vector3 motion) {
         camera.localPosition += motion;
--- Sane/Assets/src/Player/InputManager.cs
+++ Sane/Assets/src/Player/InputManager.cs
@@ -15,20 +15,13 @@ public class InputManager : MonoBehaviour {
         playerInput.Disable();
     }
 
+    // player input getters ------------------------------------------------------------------------------------------------
     public float GetPlayerMovementForward() {
         return playerInput.Movement.Forward.ReadValue<float>();
     }
 
     public float GetPlayerMovementRight() {
         return playerInput.Movement.Right.ReadValue<float>();
-    }
-
-    public bool IsPlayerRunning() {
-        return playerInput.Movement.Run.ReadValue<float>() != 0;
-    }
-
-    public bool IsPlayerCrouching() {
-        return playerInput.Movement.Crouch.ReadValue<float>() != 0;
     }
 
     public float GetMouseX() {
@@ -38,4 +31,55 @@ public class InputManager : MonoBehaviour {
     public float GetMouseY() {
         return playerInput.CameraLook.MouseY.ReadValue<float>();
     }
+
+    public bool IsRunningButtonDown() {
+        return playerInput.Movement.Run.ReadValue<float>() != 0;
+    }
+
+    public bool IsCrouchingButtonDown() {
+        return playerInput.Movement.Crouch.ReadValue<float>() != 0;
+    }
+
+
+    // player state getters ------------------------------------------------------------------------------------------------
+
+    public bool ShouldPlayerCrouch() {
+        return !IsPlayerRunning() && IsCrouchingButtonDown();
+    }
+
+    public bool ShouldPlayerRun() {
+        return !IsCrouchingButtonDown() && IsRunningButtonDown();
+    }
+
+    public bool IsPlayerMoving() {
+        return playerInput.Movement.Forward.ReadValue<float>() != 0 ||
+               playerInput.Movement.Right.ReadValue<float>() != 0;
+    }
+
+    public bool IsPlayerCrouching() {
+        return ShouldPlayerCrouch();
+    }
+
+    public bool IsPlayerWalking() {
+        return IsPlayerMoving() && !ShouldPlayerRun();
+    }
+
+    public bool IsPlayerRunning() {
+        return ShouldPlayerRun() && IsPlayerMoving();
+    }
+
+
+    public PlayerState GetPlayerState() {
+        if (IsPlayerCrouching()) return PlayerState.Crouching;
+        if (IsPlayerRunning()) return PlayerState.Running;
+        if (IsPlayerWalking()) return PlayerState.Walking;
+        return PlayerState.Idle;
+    }
+}
+
+public enum PlayerState {
+    Idle,
+    Walking,
+    Running,
+    Crouching
 }
